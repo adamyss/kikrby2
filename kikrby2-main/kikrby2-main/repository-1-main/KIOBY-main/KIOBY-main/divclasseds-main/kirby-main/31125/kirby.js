6eb990daf52d76e2460dc1e5a33cd77a3984a043
adamyss <adam.ruzicka@mcvts.org>
2025-03-24 09:52:51 -0400

    Add files via upload

---
+++ kikrby2-main/kikrby2-main/repository-1-main/KIOBY-main/KIOBY-main/divclasseds-main/kirby-main/31125/kirby.js
@@ -0,0 +1,10 @@
+let ohno = document.getElementById("ohno");
+dead();
+function dead(){
+ sigma = true
+  while (sigma == false){
+    window.open("file:///C:/Users/Compsci/Downloads/repository-1-main/repository-1-main/KIOBY-main/KIOBY-main/divclasseds-main/kirby-main/31125/index.html", "_blank")
+  }
+}
+
+
